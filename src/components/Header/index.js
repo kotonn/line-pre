85b1dbeb5d4c336f62ff54b15df8b505c4a98b29
kotonn <kotokazu68@gmail.com>
2024-03-01 08:02:00 +0900

    header-aos削除

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,24 +1,24 @@
 // Header コンポーネント
-import React, { useEffect } from "react";
+import React from "react";
 import PropTypes from "prop-types";
 import cn from "classnames";
 import { useSetRecoilState } from "recoil";
 import { userAnswerState } from "state/userAnswerState"; // 正確なパスに修正してください
 import styles from "./index.module.scss";
 import { useNavigate } from "react-router-dom";
-import AOS from "aos";
+// import AOS from "aos";
 import "aos/dist/aos.css";
 
 function Header(props) {
   const navigate = useNavigate();
   const setChartData = useSetRecoilState(userAnswerState);
 
-  useEffect(() => {
-    AOS.init({
-      duration: 500,
-      delay: 500,
-    });
-  }, []);
+  // useEffect(() => {
+  //   AOS.init({
+  //     duration: 500,
+  //     delay: 500,
+  //   });
+  // }, []);
 
   const handleHeaderClick = () => {
     setChartData({
@@ -38,7 +38,7 @@ function Header(props) {
     <div
       className={cn(styles.image1, props.className, "header")}
       onClick={handleHeaderClick}
-      data-aos="fade"
+      // data-aos="fade"
     >
       <img src={"/assets/header.svg"} alt="" />
     </div>
